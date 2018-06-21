724fc9b75a27f85c2042d37bccedb07b944a29c1
Christopher Ross <cross25@uis.edu>
2018-06-21 08:59:44 -0700

    fixed exception

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -15,7 +15,8 @@ namespace GradeBook.GradeBooks
 
         public override char GetLetterGrade(double averageGrade)
         {
-            StudentCountCheck();
+            if (Students.Count < 5)
+                throw new InvalidOperationException("Ranked grading requires at least 5 students.");
 
             var threshold = (int)Math.Ceiling(Students.Count * 0.2);
             var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
@@ -27,22 +28,24 @@ namespace GradeBook.GradeBooks
             else { return 'F'; }
         }
 
-        private void StudentCountCheck()
-        {
-            if (Students.Count < 5)
-                throw new InvalidOperationException("Ranked grading requires at least 5 students.");
-        }
-
         public override void CalculateStatistics()
         {
-            StudentCountCheck();
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+                return;
+            }
 
             base.CalculateStatistics();
         }
 
         public override void CalculateStudentStatistics(string name)
         {
-            StudentCountCheck();
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+                return;
+            }
             base.CalculateStudentStatistics(name);
         }
     }
