8e0a3bb144beff9c512466936abe703ebc7b6582
Christopher Ross <cross25@uis.edu>
2018-06-21 08:29:12 -0700

    step 4

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -15,8 +15,7 @@ namespace GradeBook.GradeBooks
 
         public override char GetLetterGrade(double averageGrade)
         {
-            if (Students.Count < 5)
-                throw new InvalidOperationException("Ranked grading requires at least 5 students.");
+            StudentCountCheck();
 
             var threshold = (int)Math.Ceiling(Students.Count * 0.2);
             var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
@@ -26,6 +25,25 @@ namespace GradeBook.GradeBooks
             else if (grades[threshold * 3 - 1] <= averageGrade) { return 'C'; }
             else if (grades[threshold * 4 - 1] <= averageGrade) { return 'D'; }
             else { return 'F'; }
+        }
+
+        private void StudentCountCheck()
+        {
+            if (Students.Count < 5)
+                throw new InvalidOperationException("Ranked grading requires at least 5 students.");
+        }
+
+        public override void CalculateStatistics()
+        {
+            StudentCountCheck();
+
+            base.CalculateStatistics();
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            StudentCountCheck();
+            base.CalculateStudentStatistics(name);
         }
     }
 }
